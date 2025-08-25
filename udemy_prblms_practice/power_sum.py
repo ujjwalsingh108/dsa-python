9b19ff9c1dfe316f6684bd072de63a67eb13b217
ujjwal-ampcome <ujjwal.singh@ampcome.com>
2025-08-25 13:47:59 +0530

    power_sum

---
+++ udemy_prblms_practice/power_sum.py
@@ -0,0 +1,10 @@
+def power_sum(array, power = 1):
+  total = 0
+  for item in array:
+    if isinstance(item, list): # check if nested list
+      total = power_sum(item, power + 1)
+    else:
+      total += item
+    
+  return total ** power
+    
